8ee5f9e2c1e11f85f526db1eb2eeb4b0ddeb40b0
MiguelCLG <miguelclg@gmail.com>
2019-01-23 21:33:35 +0000

    added games to new page to avoid music conflict with react

--- src/modules/gamedev-page.js
+++ src/modules/gamedev-page.js
@@ -13,8 +13,8 @@ export default class GameDev extends Component {
           <div className="page-wrapper">
             <div className="page-inner-wrapper">
               <div className="group">
-                <div className="game-cols span_1_of_3"><a className="font-persona text-light text-white" href="/#" target="_blank" onClick={(e) => this.props.navigation(5)}><img src={require("../images/knightlyduel.jpg")} alt="Knightly Duel" className="game-thumbnails"/> Knightly Duel</a></div>
-                <div className="game-cols span_1_of_3"><a className="font-persona text-light text-white" href="/#" onClick={(e) => this.props.navigation(6)}><img src={require("../images/spaceinvaders.jpg")} alt="Knightly Duel" className="game-thumbnails"/>Space Invaders</a></div>
+                <div className="game-cols span_1_of_3"><a className="font-persona text-light text-white" href={"KnightlyDuels/index.html"} target="_blank" rel="noopener noreferrer"><img src={require("../images/knightlyduel.jpg")} alt="Knightly Duel" className="game-thumbnails"/> Knightly Duel</a></div>
+                <div className="game-cols span_1_of_3"><a className="font-persona text-light text-white" href={"SpaceInvaders/index.html"} target="_blank" rel="noopener noreferrer"><img src={require("../images/spaceinvaders.jpg")} alt="Knightly Duel" className="game-thumbnails"/>Space Invaders</a></div>
                 <div className="game-cols span_1_of_3"><a className="font-persona text-light text-white" href="/#">Game Development</a></div>
               </div>
             </div>
