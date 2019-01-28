bcc387e09ca5e49694dd9ddb5ff31ee630e4298d
MiguelCLG <miguelclg@hotmail.com>
2019-01-28 09:31:42 +0000

    changed html from games to accomodate FullScreen and added blockbreaker

--- src/modules/gamedev-page.js
+++ src/modules/gamedev-page.js
@@ -15,7 +15,8 @@ export default class GameDev extends Component {
               <div className="group">
                 <div className="game-cols span_1_of_3"><a className="font-persona text-light text-white" href={"KnightlyDuels/index.html"} target="_blank" rel="noopener noreferrer"><img src={require("../images/knightlyduel.jpg")} alt="Knightly Duel" className="game-thumbnails"/> Knightly Duel</a></div>
                 <div className="game-cols span_1_of_3"><a className="font-persona text-light text-white" href={"SpaceInvaders/index.html"} target="_blank" rel="noopener noreferrer"><img src={require("../images/spaceinvaders.jpg")} alt="Space Invaders" className="game-thumbnails"/>Space Invaders</a></div>
-                <div className="game-cols span_1_of_3"><a className="font-persona text-light text-white" href="/#">Game Development</a></div>
+                <div className="game-cols span_1_of_3"><a className="font-persona text-light text-white" href={"BlockBreaker/index.html"} target="_blank" rel="noopener noreferrer"><img src={require("../images/blockbreaker.jpg")} alt="Block Breaker" className="game-thumbnails"/>Block Breaker</a></div>
+
               </div>
             </div>
           </div>
